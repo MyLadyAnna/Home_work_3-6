df5cdd3a1c9db9e6eeb55093265d35b79e4a66c5
Anna <ann.sxd@example.com>
2022-11-16 16:31:50 +0300

    2 задача

---
+++ 02_intersection_point/Program.cs
@@ -0,0 +1,30 @@
+﻿//  Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Clear();
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine() ?? "0");
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine() ?? "0");
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine() ?? "0");
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine() ?? "0");
+
+
+void GetIntersectionPoint(double k1, double b1, double k2, double b2)
+{
+    if (k1 == k2)
+    {
+        Console.WriteLine($"Прямые y={k1}*x + {b1} и y={k2}*x + {b2} не пересекутся, так как k1 и k2 равны");
+    }
+    else
+    {
+        double x = Math.Round((b2 - b1) / (k1 - k2), 2);             // вывела математически
+        double y = Math.Round(k1 * x + b1, 2);                       // для точности взяла double и оставила 2 знака после запятой
+        Console.WriteLine($"Точка пересечения двух прямых имеет координаты: [{x}, {y}]");
+    }
+}
+
+
+GetIntersectionPoint(k1, b1, k2, b2);
